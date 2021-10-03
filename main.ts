6b8bf648106726b1303f2a44c5185ba1425489cd
matduck <86911229+matduck@users.noreply.github.com>
2021-10-03 17:25:05 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+function AffichageLed (Forward: boolean, Backward: boolean, Stop2: boolean) {
+    SuperBit.RGB_Program().clear()
+    SuperBit.RGB_Program().setBrightness(30)
+    if (Forward) {
+        SuperBit.RGB_Program().setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        SuperBit.RGB_Program().setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    }
+    if (Backward) {
+        SuperBit.RGB_Program().setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+        SuperBit.RGB_Program().setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+    }
+    if (Stop2) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    SuperBit.RGB_Program().show()
+}
+function GoForward () {
+    AffichageLed(true, false, false)
+    basic.showIcon(IconNames.Happy)
+    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+    basic.pause(5000)
+}
 input.onButtonPressed(Button.AB, function () {
     if (Enable == 1) {
         Enable = 0
@@ -5,28 +28,40 @@ input.onButtonPressed(Button.AB, function () {
         Enable = 1
     }
 })
+function GoBackward () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    AffichageLed(false, true, false)
+    basic.showIcon(IconNames.Skull)
+    basic.pause(3000)
+}
+function Stop () {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
+    AffichageLed(false, false, true)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+}
 let Enable = 0
-Enable = 0
+SuperBit.RGB_Program().setBrightness(30)
+SuperBit.RGB_Program().setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+SuperBit.RGB_Program().setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+SuperBit.RGB_Program().setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+SuperBit.RGB_Program().setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+SuperBit.RGB_Program().show()
+basic.pause(2000)
+SuperBit.RGB_Program().clear()
 basic.forever(function () {
     if (Enable == 1) {
-        basic.showIcon(IconNames.Happy)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
-        basic.pause(5000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
-        basic.showIcon(IconNames.Sad)
-        basic.pause(1000)
-        basic.showIcon(IconNames.Skull)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
-        basic.pause(3000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, 0)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 0)
-        basic.showIcon(IconNames.Sad)
-        basic.pause(1000)
-    } else {
-        basic.showString("Off")
-        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+        GoForward()
+        Stop()
+        if (Enable == 1) {
+            GoBackward()
+            Stop()
+        }
+    }
+    if (Enable == 0) {
+        Stop()
+        basic.showString("OFF")
     }
 })
